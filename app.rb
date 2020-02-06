763cf0da598b2f438fa03bf5685a426298850c52
Logan Smith <logsmith06@gmail.com>
2020-02-06 23:05:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,11 +9,19 @@ class App < Sinatra::Base
   post '/team' do
     @name = params[:name]
     @coach = params[:coach]
+
     @pg = params[:pg]
     @sg = params[:sg]
     @sf = params[:sf]
     @pf = params[:pf]
     @c = params[:c]
+
+    @point_guard = params[:pg]
+    @shooting_guard = params[:sg]
+    @small_forward = params[:sf]
+    @power_forward = params[:pf]
+    @center = params[:c]
+
     erb :team
   end
 
